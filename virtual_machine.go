a5ce875a194aa74bcdda0a3db4f23b4c5d0b07e3
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2022-10-22 10:13:50 -0400

    Added more inst

--- virtual_machine.go
+++ virtual_machine.go
@@ -20,6 +20,7 @@ func ZVMTick(c ZVMContext) ZVMContext {
 	var inst uint32
 	var imm uint32
 	var rs1 uint32
+	var rs2 uint32
 	var rd uint32
 	var funct3 uint32
 
@@ -41,7 +42,7 @@ func ZVMTick(c ZVMContext) ZVMContext {
 		imm = ((inst & 0x80000000) >> 11) | (inst & 0xff000) | ((inst >> 9) & 0x800) | ((inst >> 20) & 0x7fe)
 
 		if (imm & 0x100000) == 0x100000 {
-			imm = imm | 0xffe00000
+			imm = imm | 0xfff00000
 		}
 
 		rd = (inst >> 7) & 0x1f
@@ -63,11 +64,82 @@ func ZVMTick(c ZVMContext) ZVMContext {
 	} else if opcode == 0b1100011 {
 		imm = ((inst & 0x80000000) >> 19) | ((inst & 0x80) << 4) | ((inst >> 20) & 0x7e0) | ((inst >> 7) & 0x1e)
 
-		funct3 = (inst >> 12) & 0x7
-		if funct3 == 0b000 {
-
+		if (imm & 0x1000) == 0x1000 {
+			imm = imm | 0xfffff000
 		}
 
+		rs1 = (inst >> 15) & 0x1f
+		rs2 = (inst >> 20) & 0x1f
+		funct3 = (inst >> 12) & 0x7
+
+		if funct3 == 0b000 {
+			// BEQ
+			if c.r[rs1] == c.r[rs2] {
+				c.pc = imm + c.pc
+			}
+		} else if funct3 == 0b001 {
+			// BNE
+			if c.r[rs1] != c.r[rs2] {
+				c.pc = imm + c.pc
+			}
+		} else if funct3 == 0b100 {
+			// BLT
+			if ((c.r[rs1] & 0x80000000) != 0x80000000) && ((c.r[rs2] & 0x80000000) != 0x80000000) {
+				if c.r[rs1] < c.r[rs2] {
+					c.pc = imm + c.pc
+				}
+			} else if ((c.r[rs1] & 0x80000000) == 0x80000000) && ((c.r[rs2] & 0x80000000) == 0x80000000) {
+				if c.r[rs1] > c.r[rs2] {
+					c.pc = imm + c.pc
+				}
+			} else if (c.r[rs1] & 0x80000000) == 0x80000000 {
+				c.pc = imm + c.pc
+			}
+		} else if funct3 == 0b101 {
+			// BGE
+			if c.r[rs1] == c.r[rs2] {
+				c.pc = imm + c.pc
+			} else if ((c.r[rs1] & 0x80000000) != 0x80000000) && ((c.r[rs2] & 0x80000000) != 0x80000000) {
+				if c.r[rs1] > c.r[rs2] {
+					c.pc = imm + c.pc
+				}
+			} else if ((c.r[rs1] & 0x80000000) == 0x80000000) && ((c.r[rs2] & 0x80000000) == 0x80000000) {
+				if c.r[rs1] < c.r[rs2] {
+					c.pc = imm + c.pc
+				}
+			} else if (c.r[rs2] & 0x80000000) == 0x80000000 {
+				c.pc = imm + c.pc
+			}
+		} else if funct3 == 0b110 {
+			// BLTU
+			if c.r[rs1] < c.r[rs2] {
+				c.pc = imm + c.pc
+			}
+		} else if funct3 == 0b111 {
+			// BGEU
+			if c.r[rs1] >= c.r[rs2] {
+				c.pc = imm + c.pc
+			}
+		}
+
+	} else if opcode == 0b0000011 {
+		rd = (inst >> 7) & 0x1f
+		rs1 = (inst >> 15) & 0x1f
+		funct3 = (inst >> 12) & 0x7
+
+		imm = inst >> 20
+
+		if (imm & 0x800) == 0x800 {
+			imm = imm | 0xfffff800
+		}
+
+		if funct3 == 0b000 {
+			// LB
+			c.r[rd] = uint32(c.m[c.r[rs1]+imm])
+			if (c.r[rd] & 0x80) == 0x80 {
+				c.r[rd] = c.r[rd] | 0xffffff80
+			}
+		}
 	}
 
 	fmt.Println(opcode, inst, imm, rs1, rd)
