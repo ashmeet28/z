ad40603ec5449384dd3ab771a87c0a8d6a511d28
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2022-10-22 08:46:53 -0400

    Added more code

--- virtual_machine.go
+++ virtual_machine.go
@@ -21,6 +21,9 @@ func ZVMTick(c ZVMContext) ZVMContext {
 	var imm uint32
 	var rs1 uint32
 	var rd uint32
+	var funct3 uint32
+
+	c.r[0] = 0
 
 	inst = uint32(c.m[c.pc]) | uint32(c.m[c.pc+1])<<8 | uint32(c.m[c.pc+2])<<16 | uint32(c.m[c.pc+3])<<24
 	opcode = inst & 0x7f
@@ -33,6 +36,38 @@ func ZVMTick(c ZVMContext) ZVMContext {
 		// AUIPC
 		rd = (inst >> 7) & 0x1f
 		c.r[rd] = (inst & 0xfffff000) + c.pc
+	} else if opcode == 0b1101111 {
+		// JAL
+		imm = ((inst & 0x80000000) >> 11) | (inst & 0xff000) | ((inst >> 9) & 0x800) | ((inst >> 20) & 0x7fe)
+
+		if (imm & 0x100000) == 0x100000 {
+			imm = imm | 0xffe00000
+		}
+
+		rd = (inst >> 7) & 0x1f
+		c.r[rd] = c.pc + 4
+		c.pc = c.pc + imm
+	} else if opcode == 0b1100111 {
+		// JALR
+		rd = (inst >> 7) & 0x1f
+		rs1 = (inst >> 15) & 0x1f
+		imm = inst >> 20
+
+		if (imm & 0x800) == 0x800 {
+			imm = imm | 0xfffff000
+		}
+
+		c.r[rd] = c.pc + 4
+		c.pc = (imm + c.r[rs1]) & 0xfffffffe
+
+	} else if opcode == 0b1100011 {
+		imm = ((inst & 0x80000000) >> 19) | ((inst & 0x80) << 4) | ((inst >> 20) & 0x7e0) | ((inst >> 7) & 0x1e)
+
+		funct3 = (inst >> 12) & 0x7
+		if funct3 == 0b000 {
+
+		}
+
 	}
 
 	fmt.Println(opcode, inst, imm, rs1, rd)
