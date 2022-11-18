87d5891f854a77a71eb64e6a581a8f60c9043ecb
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2022-11-18 18:54:07 -0500

    Added assembler

--- assembler.go
+++ assembler.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+func ASMGetLines(data []uint8) [][]uint8 {
+	var Lines [][]uint8
+	var LineLength int
+	var LastIndex int
+	LastIndex = 0
+	for i := range data {
+		if data[i] == 0x0a {
+			if LineLength != 0 {
+				Lines = append(Lines, data[LastIndex:(LastIndex+LineLength)])
+			}
+			LastIndex = i + 1
+			LineLength = 0
+		} else {
+			LineLength++
+		}
+	}
+	return Lines
+}
 func main() {
-	fmt.Println("Hello, World!")
+	d := []uint8{0x0a, 0x0a, 2, 5, 7, 9, 0x0a, 0x0a, 12, 55, 0x0a, 0x0a}
+	fmt.Println(ASMGetLines(d))
 }
